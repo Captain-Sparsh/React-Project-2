3d5e012661cf5618aaacff65ae0a3a791b8151da
Sparsh <sparshranjan01@gmail.com>
2023-07-09 20:45:39 +0530

    Fix: Images

--- src/components/RoleDice/RoleDIce.jsx
+++ src/components/RoleDice/RoleDIce.jsx
@@ -5,7 +5,7 @@ const RoleDIce = (props) => {
   return (
     <DiceContainer>
       <div className='dice' onClick={props.rollDice}>
-        <img src = {`src/assets/dice_${props.diceNumber}.png`} alt= "dice 4" />
+        <img src = {`../../../src/assets/dice_${props.diceNumber}.png`} alt= "dice 4" />
       </div>
       <p>Click on the Dice to roll</p>
     </DiceContainer>
